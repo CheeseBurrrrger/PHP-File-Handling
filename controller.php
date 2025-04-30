74c82fc6e31c1c2cb2147fa0981c00c36be533ce
Dooody <dodysyahputra@student.ub.ac.id>
2025-05-01 01:38:36 +0700

    Solved Case #2: cleaned up and simplified the method. Note: HTML remains unstyled.

--- controller.php
+++ controller.php
@@ -1,60 +1,41 @@
 <?php
-// echo "<= ini pake readfile ".readfile('testing.txt'); // ini disamping buat baca konten dari file dan ada jumlah karakter
-
-// $tess = fopen('testing.txt','r');
-// echo "<br>";
-// $filesize = filesize('testing.txt');
-// echo "ini coba pake fread => ". fread($tess,$filesize)."<br>";
-// fclose($tess);
-
-//coba search
-
-// $search = 'bmw';
-// $file = fopen('testing.txt','r');
-// $filesize = filesize('testing.txt');
-// $text = fread($file,$filesize);
-// echo $text;
-
-// echo "<br>ini nyoba count = ".substr_count($text,'bmw')."<br><br>";
-// if (str_contains($text,$search)){
-//     $newstring = str_replace($search,'<em>mercedes</em>',$text." ini setelahnya ");
-// };
-// echo "<br><br>!!".$newstring;
-// echo "<br><br>coba substring ".substr_count($text,'coba');
-// echo fread($file,$filesize);
-// file_put_contents('testing.txt',$newstring);
-// echo "<br><br>ini read setelah edit ".fread($file,$filesize);
-// echo "<br><br>".$text;
-
-
-//•To write data to a file, use fwrite()
-
-// $file_handle = fopen('article.txt', 'r+');
-// fwrite($file_handle, 'A string data');
-
-// file_put_contents(filename, data, mode, context) //source w3schools
-
-// lets make it more clean
-
-$search = 'bmw';
-$replace = 'bmw m-series';
-$fileread = fopen('testing.txt','r+');
-$size = filesize('testing.txt');
-$text = fread($fileread,$size);
-echo $text.'<br>';
-$newstring = str_replace($search,$search.' terus kuganti <em>'.$replace.'</em>',$text);
-$finalstring = str_replace('awalnya '.$search,'jadi <em>'.$replace.'</em>',$text);
-echo $newstring.'<br>';
-echo $finalstring.'<br>';
-
-file_put_contents('testing.txt',$finalstring);
-
-echo 'ini print filenya => '.$text;
-
-    function readOnly(){
-        $file = fopen('testing.txt','r');
-        $text = file_get_contents('testing.txt');
-        return $text;
+class controller{
+    function refinedCase2(){
+        $namafile = $_POST['namafile'];
+        $keyword = $_POST['keyword'];
+        $operasi = $_POST['operasi'];
+        $output = $_POST['output'];
+        $modewrite = ($output=='N'||$output=='n')? $modewrite='w': $modewrite='r+';
+        ($modewrite=='w')? $newnamafile = str_replace('.txt','',$namafile).'-new.txt':$newnamafile=$namafile;
+        $readFile = (file_exists($namafile))?fopen($namafile,'r'):die('unfortunately we cannot find the file:(');
+        echo '<br>isi konten dalam file => '.file_get_contents($namafile).'<br>';
+        $content = file_get_contents($namafile);
+        $found = array();
+        $baris = 1;
+        if ($readFile){
+            while (!feof($readFile)){
+                $temp = fgets($readFile);
+                if(strpos($temp,$keyword)!==false){
+                    if($operasi=='redact'){
+                        $found[$baris] = str_replace($keyword,str_repeat('*', strlen($keyword)),$temp); 
+                    }
+                    else{
+                        $found[$baris] = str_replace($keyword,'<em>'.$keyword.'</em>',$temp);
+                    }
+                }
+                $baris++;
+            }          
+        }
+        fclose($readFile);
+        foreach ($found as $index => $line) {
+            echo '<br>kata '.$keyword.' ditemukan pada baris ke-'.($index) . " " . $line ;
+        }
+        $content = ($operasi=='redact')? $content = str_replace($keyword,str_repeat('*', strlen($keyword)),$content) : $content = str_replace($keyword,'<em>'.$keyword.'</em>',$content);
+        file_put_contents($newnamafile,$content);
+        return '<br><br>isi konten setelah di edit sebagai berikut: <br>'.file_get_contents($newnamafile);
     }
-
-echo readOnly();
+}
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $case2 = new controller();
+    echo $case2->refinedCase2();
+}
---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Case 2</title>
+    <script src="jquery.js"></script>
+    <script src="script.js"></script>
+
+</head>
+<body>
+    <div>
+        <form method="post">
+            <input type="text" name="namafile" id="namafile" placeholder="nama file">
+            <input type="text" name="keyword" id="keyword" placeholder="keyword">
+            <input type="text" name="operasi" id="operasi" placeholder="operasi">
+            <input type="text" name="output" id="output" placeholder="output">
+            <button id="sbm-btn">submit</button>
+        </form>
+    </div>
+</body>
+</html>
